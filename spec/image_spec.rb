3db5981a74d3c9aea1064926978c46241cea8917
Dario <darios3@gmail.com>
2011-02-28 20:08:17 -0300

    created basic spec for image

---
+++ spec/image_spec.rb
@@ -0,0 +1,43 @@
+require "lib/imageruby"
+
+include ImageRuby
+
+describe Image, "Image" do
+
+  def self.test_create(width, height)
+    it "should create images of #{width}x#{height}" do
+      image = Image.new(width,height)
+
+      image.width.should be == width
+      image.height.should be == height
+    end
+  end
+
+  def self.test_create_and_black(width, height)
+    it "should create images of #{width}x#{height} and should be black for all pixels" do
+      image = Image.new(width,height)
+
+      (0..image.width-1).each do |x|
+        (0..image.height-1).each do |y|
+          c = image[x,y]
+          c.red.should be == 0
+          c.green.should be == 0
+          c.blue.should be == 0
+        end
+      end
+    end
+  end
+
+  test_create(10,10)
+  test_create(0,0)
+  test_create(640,480)
+  test_create(1,100)
+  test_create(20,67)
+
+  test_create_and_black(10,10)
+  test_create_and_black(0,0)
+  test_create_and_black(640,480)
+  test_create_and_black(1,100)
+  test_create_and_black(20,67)
+
+end
