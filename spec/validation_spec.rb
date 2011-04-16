fb3fb479a49cb6ea6d537a53aa1a9326340a1c77
Dario <darios3@gmail.com>
2011-04-16 18:32:46 -0300

    fixed assertion of OutOfRange exception

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -9,33 +9,33 @@ describe Color, "Color" do
       it "should validate out of range color channels with value #{value} when create with Color.from_rgb" do
         lambda {
           Color.from_rgb(value,0,0)
-        }.should_raise Color::OutOfRangeException
+        }.should raise_error(Color::OutOfRangeException)
       end
 
       it "should validate out of range color channels with value #{value} when create with Color.from_rgba" do
         lambda {
           Color.from_rgba(value,0,0,0)
-        }.should_raise Color::OutOfRangeException
+        }.should raise_error(Color::OutOfRangeException)
       end
 
       it "should validate out of range color channel alpha with value #{value} when create with Color.from_rgba" do
         lambda {
           Color.from_rgba(0,0,0,value)
-        }.should_raise Color::OutOfRangeException
+        }.should raise_error(Color::OutOfRangeException)
       end
 
       it "should validate out of range color channel with value #{value} when assign red channel" do
         color = Color.from_rgb(0,0,0)
         lambda {
           color.r = value
-        }.should_raise Color::OutOfRangeException
+        }.should raise_error(Color::OutOfRangeException)
       end
 
       it "should validate out of range color channel with value #{value} when assign alpha channel" do
         color = Color.from_rgb(0,0,0)
         lambda {
           color.a = value
-        }.should_raise Color::OutOfRangeException
+        }.should raise_error(Color::OutOfRangeException)
       end
     end
   end
