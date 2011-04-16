7e815cbff2244e0f20887f30a2c1c5a326772c02
Dario <darios3@gmail.com>
2011-04-16 18:39:55 -0300

    aded validation specs for green and blue channels

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -6,21 +6,15 @@ describe Color, "Color" do
 
   def self.validate_out_of_range(*values)
     values.each do |value|
-      it "should validate out of range color channels with value #{value} when create with Color.from_rgb" do
+      it "should validate out of range color channels with red=#{value} when create with Color.from_rgb" do
         lambda {
           Color.from_rgb(value,0,0)
         }.should raise_error(Color::OutOfRangeException)
       end
 
-      it "should validate out of range color channels with value #{value} when create with Color.from_rgba" do
+      it "should validate out of range color channels with red=#{value} when create with Color.from_rgba" do
         lambda {
           Color.from_rgba(value,0,0,0)
-        }.should raise_error(Color::OutOfRangeException)
-      end
-
-      it "should validate out of range color channel alpha with value #{value} when create with Color.from_rgba" do
-        lambda {
-          Color.from_rgba(0,0,0,value)
         }.should raise_error(Color::OutOfRangeException)
       end
 
@@ -28,6 +22,51 @@ describe Color, "Color" do
         color = Color.from_rgb(0,0,0)
         lambda {
           color.r = value
+        }.should raise_error(Color::OutOfRangeException)
+      end
+
+      it "should validate out of range color channels with green=#{value} when create with Color.from_rgb" do
+        lambda {
+          Color.from_rgb(0,value,0)
+        }.should raise_error(Color::OutOfRangeException)
+      end
+
+      it "should validate out of range color channels with green=#{value} when create with Color.from_rgba" do
+        lambda {
+          Color.from_rgba(0,value,0,0)
+        }.should raise_error(Color::OutOfRangeException)
+      end
+
+      it "should validate out of range color channel with value #{value} when assign green channel" do
+        color = Color.from_rgb(0,0,0)
+        lambda {
+          color.g = value
+        }.should raise_error(Color::OutOfRangeException)
+      end
+
+      it "should validate out of range color channels with blue=#{value} when create with Color.from_rgb" do
+        lambda {
+          Color.from_rgb(0,0,value)
+        }.should raise_error(Color::OutOfRangeException)
+      end
+
+      it "should validate out of range color channels with blue=#{value} when create with Color.from_rgba" do
+        lambda {
+          Color.from_rgba(0,0,value,0)
+        }.should raise_error(Color::OutOfRangeException)
+      end
+
+      it "should validate out of range color channel with value #{value} when assign blue channel" do
+        color = Color.from_rgb(0,0,0)
+        lambda {
+          color.b = value
+        }.should raise_error(Color::OutOfRangeException)
+      end
+
+
+      it "should validate out of range color channel alpha with value #{value} when create with Color.from_rgba" do
+        lambda {
+          Color.from_rgba(0,0,0,value)
         }.should raise_error(Color::OutOfRangeException)
       end
 
