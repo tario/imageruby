5f21c17634592eda383eddde966c0a40aa7ab86b
Dario <darios3@gmail.com>
2011-05-12 22:26:24 -0300

    created filepersistor and fileloader

---
+++ lib/imageruby/filepersistor.rb
@@ -0,0 +1,40 @@
+=begin
+
+This file is part of the imageruby project, http://github.com/tario/imageruby
+
+Copyright (c) 2011 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+imageruby is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+imageruby is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with imageruby.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
+require "imageruby/abstract/subclass_enum"
+
+module ImageRuby
+  class FilePersistor
+    with_enumerable_subclasses
+
+    def self.persist(image,path,format)
+      FilePersistor.each_subclass do |sc|
+        persistor = sc.new
+
+        begin
+          return persistor.persist(image,path,format)
+        rescue UnableToPersistException
+        end
+      end
+
+      raise UnableToPersistException
+    end
+  end
+end
